9e5d4ec53e310eccee4cd189f4c06a662fc1d750
Peter van der Vlugt <Petorrr@users.noreply.github.com>
2017-07-02 10:23:10 +0200

    Created repository structure

---
+++ sketch_3sss/sketch_3sss.ino
@@ -0,0 +1,24 @@
+//#include <bluefruit.h>
+#include <stdio.h>
+
+#define STRING_BUF_SIZE     80
+
+static char TmpBuf [STRING_BUF_SIZE];
+
+void setup()
+{
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+  Serial.println ("Hello World");
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+  sprintf (TmpBuf, "Millis since start: %ul msec", millis());
+}
