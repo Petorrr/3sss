0a3f155adc83b98ca4023219df725dd09c6f5e63
Peter van der Vlugt <Petorrr@users.noreply.github.com>
2017-07-02 15:40:26 +0200

    Add files via upload

--- sketch_3sss/sketch_3sss.ino
+++ sketch_3sss/sketch_3sss.ino
@@ -17,7 +17,7 @@ void loop()
 {
   // put your main code here, to run repeatedly:
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
+  delay(500);                        // wait for half a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
   sprintf (TmpBuf, "Millis since start: %ul msec", millis());
