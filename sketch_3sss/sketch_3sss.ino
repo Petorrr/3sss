d3f85e043b95ea3982c4808c698e11b28d83bb3b
Peter van der Vlugt <Petorrr@users.noreply.github.com>
2017-07-29 17:49:45 +0200

    Working prototype with SoftwareSerial and ANT.cpp

--- sketch_3sss/sketch_3sss.ino
+++ sketch_3sss/sketch_3sss.ino
@@ -13,13 +13,24 @@
 #define STRING_BUF_SIZE     80
 #define PIEZO_BUF_SIZE      20
 #define HX711_BUF_SIZE      20
+#define HX711_DATA_PIN      (7)
+#define HX711_CLOCK_PIN     (11)
 #define ANA_RES_BITS        14
 #define ANA_INP_RANGE       16384
 #define ANA_VOLT_REF        (float) 3.6
 
+// ANT AP2 Interface definitions
+#define AP2_TX_PIN          (4)
+#define AP2_RX_PIN          (5)
+#define AP2_BR1_PIN         (28)
+#define AP2_BR2_PIN         (29)
+#define AP2_BR3_PIN         (12)
+#define AP2_RST_PIN         (13)
+#define AP2_SUSPEND_PIN     (14)
+#define AP2_SLEEP_PIN       (16)
+#define AP2_RTS_PIN         (15)
+
 // ANT Bike Power Profile definitions
-#define AP2_TX_PIN          4
-#define AP2_RX_PIN          5
 #define BIKE_POWER_PROFILE  0x0B
 #define BIKE_POWER_DEVICE   0x0001
 #define BIKE_POWER_RF       57
@@ -39,8 +50,8 @@ static byte PiezoIndex = 0;
 static word PiezoSensorVal = 0;
 
 // Loadcell, Strain Gauge Amplifier variables
-const byte  hx711_data_pin = (7);
-const byte  hx711_clock_pin = (11);
+const byte  hx711_data_pin = HX711_DATA_PIN;
+const byte  hx711_clock_pin = HX711_CLOCK_PIN;
 static long Hx711Buf [HX711_BUF_SIZE];
 static byte Hx711Index = 0;
 static long Hx711SensorVal = 0;
@@ -49,7 +60,7 @@ static long Hx711SensorVal = 0;
 static HX711 hx711(hx711_data_pin, hx711_clock_pin, 128);
 
 // ANT communication data
-static SoftwareSerial AntSerial (AP2_RX_PIN, AP2_TX_PIN);
+static SoftwareSerial AntSerial (AP2_RX_PIN, AP2_TX_PIN, false);
 static Ant            ant = Ant();
 // ANT Developer key, if you want to connect to ANT+, you must get the key from thisisant.com
 static const byte     NETWORK_KEY[] = {0xB9, 0xA5, 0x21, 0xFB, 0xBD, 0x72, 0xC3, 0x45};
@@ -64,30 +75,67 @@ static word           AntPower;
 // *****************************************************************************
 static void antSetup();
 static void readSensorsTask();
-static void parseMessage() ;
+static void parseAntMessage() ;
 static void parseEventMessage(uint8_t code);
+static void AP2reset (void);
 
 // *****************************************************************************
 // Initialization and Board setup routines
 // *****************************************************************************
 void setup()
 {
+byte i;
+
   // Put setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(LED_BLUE, OUTPUT);
 
   Serial.begin(115200);
   Serial.println ("Hello 3sss");
+
+  // Initialize the AP2 interface pins
+  pinMode (AP2_BR1_PIN, OUTPUT);
+  pinMode (AP2_BR2_PIN, OUTPUT);
+  pinMode (AP2_BR3_PIN, OUTPUT);
+  digitalWrite(AP2_RST_PIN, 1);
+  pinMode (AP2_RST_PIN, OUTPUT);
+  pinMode (AP2_SUSPEND_PIN, OUTPUT);
+  pinMode (AP2_SLEEP_PIN, OUTPUT);
+  pinMode (AP2_RTS_PIN, INPUT);
   
-  AntSerial.begin(9600);
+  // Baudrate selection to 38400 baud
+  digitalWrite(AP2_BR1_PIN, 1);
+  digitalWrite(AP2_BR2_PIN, 0);
+  digitalWrite(AP2_BR3_PIN, 0);
+  digitalWrite(AP2_RST_PIN, 1);
+  digitalWrite(AP2_SLEEP_PIN, 0);
+  digitalWrite(AP2_SUSPEND_PIN, 1);
+  // Initalize SoftwareSerial
+  AntSerial.begin(38400);
+
+  // Issue an AP2 hard reset
+  AP2reset ();
+  // Wait for 3 seconds to AP2 startup
+  Serial.println ("Waiting for reset");
+  for (i=0; i < 30; i++)
+  {
+    delay (100);
+    Serial.print (".");
+  }
+  
+  Serial.println ("");
+  Serial.println ("Starting antSetup");
   antSetup ();
-  
+  Serial.println ("End of Setup");
+  if (digitalRead (AP2_RTS_PIN))
+    Serial.println ("RTS=1");
+   else
+    Serial.println ("RTS=0");
+
   // Set the AD resolution to 14 bits
   analogReadResolution (ANA_RES_BITS);
   // Create readSensors task using Scheduler to run in 'parallel' with main loop()
   Scheduler.startLoop(readSensorsTask);
-
-  Serial.println ("End of Setup");
 }
 
 
@@ -126,8 +174,7 @@ BroadcastMsg bm;
   //sprintf (TmpBuf, "Piezo: %u; hx711 %u; ", (word) piezoMax, (word) Hx711SensorVal);
   //sprintf (TmpBuf, "LC: %ul", Hx711SensorVal & 0x007FFFFF);
   //Serial.println (TmpBuf);
-  Serial.println (Hx711SensorVal & 0x007FFFFF);
-  
+  //Serial.println (Hx711SensorVal & 0x007FFFFF);
   //Serial.println (piezoV);
   //Serial.println (piezoMax);
 
@@ -150,6 +197,11 @@ BroadcastMsg bm;
   bm.setData(AntBuf);
   bm.setChannel(0);
   ant.send(bm);
+  sprintf (TmpBuf, "Pdl %u; Cdn %u; TotPwr %u Pwr: %u", AntPedalPower, AntCadence, AntAccuPower, AntPower);
+  //Serial.println (TmpBuf);
+
+  // Parse any incoming ANT messages
+  parseAntMessage();
 
   // Run this task at 10 Hz
   delay (100);
@@ -200,19 +252,21 @@ ChannelRfFrequency crf;
 OpenChannel        oc;
 
   ant.setSerial(AntSerial);
+  //ant.setSerial(Serial);
+  
   // Send reset to ANT Network
   ant.send(rs);
   // Delay after resetting the radio to give the user time to connect on serial
   delay(1000);
   Serial.println("ANT Reset");
-  parseMessage();
+  parseAntMessage();
 
   // Set the Network Key (currently to DEVELOPER)
   snk = SetNetworkKey();
   snk.setNetwork(0);
   snk.setKey((uint8_t*)NETWORK_KEY);
   ant.send(snk);
-  parseMessage();
+  parseAntMessage();
 
   // Assign ANT Channel as bi-directional transmitter (Master)
   ac = AssignChannel();
@@ -220,7 +274,7 @@ OpenChannel        oc;
   ac.setChannelType(CHANNEL_TYPE_BIDIRECTIONAL_TRANSMIT); //can't wildcard this
   ac.setChannelNetwork(0);
   ant.send(ac);
-  parseMessage();
+  parseAntMessage();
 
   ci = ChannelId();
   ci.setChannel(0);
@@ -228,33 +282,38 @@ OpenChannel        oc;
   ci.setDeviceType(BIKE_POWER_PROFILE);
   ci.setTransmissionType(BIKE_POWER_TT);
   ant.send(ci);
-  parseMessage();
+  parseAntMessage();
 
   crf = ChannelRfFrequency();
   crf.setChannel(0);
   crf.setRfFrequency(BIKE_POWER_RF); //can't wildcard this
   ant.send(crf);
-  parseMessage();
+  parseAntMessage();
 
   cp = ChannelPeriod();
   cp.setChannel(0);
   cp.setPeriod(BIKE_POWER_CP); //can't wildcard this
   ant.send(cp);
-  parseMessage();
+  parseAntMessage();
 
   oc = OpenChannel();
   oc.setChannel(0);
   ant.send(oc);
-  parseMessage();
+  parseAntMessage();
 }
 
-static void parseMessage() 
+// *****************************************************************************
+// ANT stack message parsing
+// *****************************************************************************
+static void parseAntMessage() 
 {
-#if 0
+byte msgId;
+byte *framePtr;
+
   ant.readPacket();
   if(ant.getResponse().isAvailable())
   {
-    uint8_t msgId = ant.getResponse().getMsgId();
+    msgId = ant.getResponse().getMsgId();
     switch (msgId) {
       case CHANNEL_EVENT:
       {
@@ -278,8 +337,12 @@ static void parseMessage()
       }
 
       default:
-        Serial.print("Undefined Message: ");
+        Serial.print("Ant Message: ");
         Serial.println(msgId, HEX);
+        framePtr = ant.getResponse().getFrameData();
+        sprintf (TmpBuf, "Ant msg: %02X %02X %02X %02X", framePtr[0], framePtr[1], framePtr[2], framePtr[3]);
+        Serial.println (TmpBuf);
+        
         break;
     }
   }
@@ -288,15 +351,17 @@ static void parseMessage()
     Serial.print("ANT MSG ERROR: ");
     Serial.println(ant.getResponse().getErrorCode());
   }
-#endif
 
   delay (10);
 }
 
+// *****************************************************************************
+// ANT stack event message parsing
+// *****************************************************************************
 void parseEventMessage(uint8_t code)
 {
-#if 0
- BroadcastMsg bm;
+BroadcastMsg bm;
+
   Serial.print("Code: ");
   switch (code)
   {
@@ -310,16 +375,28 @@ void parseEventMessage(uint8_t code)
 
     case EVENT_TX:
       Serial.println("EVENT_TX");
+#if 0
       buffer[0]++;
       bm = BroadcastMsg();
       bm.setData(buffer);
       bm.setChannel(0);
       ant.send(bm);
+#endif
       break;
 
     default:
       Serial.println(code);
       break;
   }
-#endif
+}
+
+// *****************************************************************************
+// ANT AP2 hard reset function
+// *****************************************************************************
+static void AP2reset (void)
+{
+  // Issue and AP2 hard reset
+  digitalWrite(AP2_RST_PIN, 0);
+  delay (10);
+  digitalWrite(AP2_RST_PIN, 1);
 }
