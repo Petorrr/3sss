5efff817d7bcecf05aa6969523bc44c565fef64c
Peter van der Vlugt <Petorrr@users.noreply.github.com>
2017-07-31 23:58:35 +0200

    Full standalone version
    Robust and independent of ant library. Uses slightly modified SoftwareSerial

--- sketch_3sss/sketch_3sss.ino
+++ sketch_3sss/sketch_3sss.ino
@@ -5,7 +5,6 @@
 #include <HX711.h>
 #include <stdio.h>
 #include "src\SoftwareSerial.h"
-#include <ANT.h>
 
 // *****************************************************************************
 // DEFINITIONS Section
@@ -31,8 +30,10 @@
 #define AP2_RTS_PIN         (15)
 
 // ANT Bike Power Profile definitions
+#define ANT_PUBLIC_NETWORK  0x00
 #define ANT_CHANNEL_NR      0x00
-#define ANT_DEVICE_NR       0x3555
+#define ANT_DEVICE_NR       3555      //3SSS :)
+
 #define BIKE_POWER_PROFILE  0x0B
 #define BIKE_POWER_DEVICE   0x0001
 #define BIKE_POWER_RF       57
@@ -40,21 +41,22 @@
 #define BIKE_POWER_CP       8182
 
 // ANT Protocol Message definitions
-#define ANT_RXBUF_SIZE                   40
+#define ANT_RXBUF_SIZE                40
+#define ANT_RX_MAX_TIMEOUT            100 // In milliseconds
 // ANT Transmit Message definitions
-#define MSG_TX_SYNC                      ((byte)0xA4)
-#define MSG_SYSTEM_RESET_ID              ((byte)0x4A)
-#define MSG_NETWORK_KEY_ID               ((byte)0x46)
-#define MSG_ASSIGN_CHANNEL_ID            ((byte)0x42)
-#define MSG_CHANNEL_ID_ID                ((byte)0x51)
-#define MSG_CHANNEL_RADIO_FREQ_ID        ((byte)0x45)
-#define MSG_CHANNEL_MESG_PERIOD_ID       ((byte)0x43) // Set channel period 0x43
-#define MSG_RADIO_TX_POWER_ID            ((byte)0x47) // Set Tx Power 0x47
-#define MSG_CHANNEL_SEARCH_TIMEOUT_ID    ((byte)0x44) // Set Channel Search Timeout 0x44
-#define MSG_OPEN_CHANNEL_ID              ((byte)0x4B) // ID Byte 0x4B
-#define MSG_BROADCAST_DATA_ID            ((byte)0x4E)
-#define MSG_CHANNEL_RESPONSE             ((byte)0x40)
-#define MSG_TX_EVENT                     ((byte)0x03)
+#define MSG_TX_SYNC                   ((byte)0xA4)
+#define MSG_SYSTEM_RESET_ID           ((byte)0x4A)
+#define MSG_NETWORK_KEY_ID            ((byte)0x46)
+#define MSG_ASSIGN_CHANNEL_ID         ((byte)0x42)
+#define MSG_CHANNEL_ID_ID             ((byte)0x51)
+#define MSG_CHANNEL_RADIO_FREQ_ID     ((byte)0x45)
+#define MSG_CHANNEL_MESG_PERIOD_ID    ((byte)0x43)
+#define MSG_RADIO_TX_POWER_ID         ((byte)0x47)
+#define MSG_CHANNEL_SEARCH_TIMEOUT_ID ((byte)0x44)
+#define MSG_OPEN_CHANNEL_ID           ((byte)0x4B)
+#define MSG_BROADCAST_DATA_ID         ((byte)0x4E)
+#define MSG_CHANNEL_RESPONSE          ((byte)0x40)
+#define MSG_TX_EVENT                  ((byte)0x03)
 
 // ANT Channel Type Codes
 #define CHANNEL_TYPE_BIDIRECTIONAL_RECEIVE    0x00
@@ -70,6 +72,7 @@
 static char TmpBuf [STRING_BUF_SIZE];
 static byte MainCount = 0;
 
+// Piezo sensor variables
 static word PiezoBuf [PIEZO_BUF_SIZE];
 static byte PiezoSensorCh = A0;
 static byte PiezoIndex = 0;
@@ -87,10 +90,8 @@ static HX711 hx711 = HX711(hx711_data_pin, hx711_clock_pin, 128);
 
 // ANT communication data
 static SoftwareSerial AntSerial = SoftwareSerial(AP2_RX_PIN, AP2_TX_PIN, false);
-static Ant            ant = Ant();
 // ANT Developer key, if you want to connect to ANT+, you must get the key from thisisant.com
 static const byte     NETWORK_KEY[] = {0xB9, 0xA5, 0x21, 0xFB, 0xBD, 0x72, 0xC3, 0x45};
-static byte           AntBuf [8] = {0, 0, 0, 0, 0, 0, 0, 0};
 static byte           AntPedalPower;
 static byte           AntCadence;
 static word           AntAccuPower;
@@ -106,17 +107,20 @@ static byte           SendCount = 0;
 // *****************************************************************************
 // Local Function prototypes 
 // *****************************************************************************
-static void antSetup();
-static void broadcastBikePower (void);
 static void readSensorsTask();
-static void parseAntMessage() ;
-static void parseEventMessage(uint8_t code);
+
+static void broadcastBikePower (void);
+static void antSetup();
+
 static void AP2reset (void);
 static bool AP2waitCts (void);
+
 static boolean ANTreceive (word timeout);
-static void ANTrxProcess (void);
+static void    ANTrxProcess (void);
+
 static void ANTsend (uint8_t buf[], int length);
 static byte checkSum (byte *dataPtr, int length);
+
 static void ANTreset (void);
 static void ANTsetNetwork (void);
 static void ANTassignChannel (byte chNr, byte chType);
@@ -156,16 +160,15 @@ byte i;
   digitalWrite(AP2_RST_PIN, 1);
   digitalWrite(AP2_SLEEP_PIN, 0);
   digitalWrite(AP2_SUSPEND_PIN, 1);
+
   // Initalize SoftwareSerial
   AntSerial.begin(38400);
+  // Stabilize for a while
   delay (10);
-
   // Issue an AP2 hard reset
   AP2reset ();
- 
-  Serial.println ("Starting antSetup");
+  // Setup and initialize the ANT network
   antSetup ();
-  Serial.println ("End of antSetup");
 
   // Set the AD resolution to 14 bits
   analogReadResolution (ANA_RES_BITS);
@@ -183,13 +186,14 @@ byte  i;
 long  piezoVal;
 long  piezoMax;
 float piezoV;
-
-BroadcastMsg bm;
+uint32_t startMillis;
+word     execMillis;
 
   // Main loop for serial output and debugging tasks:
-
+  startMillis = millis();
+  MainCount++;
   // Toggle Run Led every 500 msec
-  if (MainCount % 5 == 0)
+  if (MainCount % 10 == 0)
     digitalWrite(LED_BUILTIN, !digitalRead (LED_BUILTIN));
   
   // Calculate moving average of Piezo Sensor values (20 samples @ 20 Hz)
@@ -218,48 +222,30 @@ BroadcastMsg bm;
   AntCadence = 93;
   AntPower = 300 + random (-25, 100);
   AntAccuPower += AntPower;
-  if ((MainCount % 3) == 0)
-  {
-    broadcastBikePower();
-    ANTreceive (100);
-  }
-  
-#if 0  
-  // Fill ANT Bike Power data buffer with data
-  AntBuf[0] = 0x10;
-  AntBuf[1] = MainCount;
-  AntBuf[2] = AntPedalPower;
-  AntBuf[3] = AntCadence;
-  AntBuf[4] = lowByte (AntAccuPower);
-  AntBuf[5] = highByte (AntAccuPower);
-  AntBuf[6] = lowByte (AntPower);
-  AntBuf[7] = highByte (AntPower);
-  // Finally broadcast the ANT Bike Power Data message
-  bm = BroadcastMsg();
-  bm.setData(AntBuf);
-  bm.setChannel(0);
-  ant.send(bm);
-  AP2waitCts();
   //sprintf (TmpBuf, "Pdl %u; Cdn %u; TotPwr %u Pwr: %u", AntPedalPower, AntCadence, AntAccuPower, AntPower);
   //Serial.println (TmpBuf);
-#endif
+  if ((MainCount % 5) == 0)
+    broadcastBikePower();
 
-  // Parse any incoming ANT messages
-  //parseAntMessage();
+  // Run ANT RX parser check with no timeout for incoming requests
+  ANTreceive (0);
 
-  // Run this task at 10 Hz
-  delay (100);
-  MainCount++;
+  // Run this task at 20 Hz
+  execMillis = millis() - startMillis;
+  if (execMillis < 50)
+    delay (50-execMillis);
 }
 
 
 // *****************************************************************************
-// Sensors Input Task samples at 50 msec
+// Sensors Input Task samples at 10 msec
 // *****************************************************************************
 static void readSensorsTask()
 {
-  // Toggle the sensor task LED
-  digitalWrite(LED_BLUE, !digitalRead (LED_BLUE));
+uint32_t startMillis;
+word     execMillis;
+
+  startMillis = millis();
 
   // Read the value from the Piezo sensor into the sample buffer
   PiezoSensorVal = analogRead (PiezoSensorCh);
@@ -279,105 +265,48 @@ static void readSensorsTask()
   }
 
   // Run this task at 100 Hz
-  delay (10);
+  execMillis = millis() - startMillis;
+  if (execMillis < 10)
+    delay (10-execMillis);
 }
 
 // *****************************************************************************
-// ANT stack initialization
+// ANT stack initialization and network setup
 // *****************************************************************************
 static void antSetup()
 {
-AssignChannel      ac;
-ResetSystem        rs;
-SetNetworkKey      snk;
-ChannelId          ci;
-ChannelPeriod      cp;
-ChannelRfFrequency crf;
-OpenChannel        oc;
-
-  ant.setSerial(AntSerial);
-  delay (10);
+  // Flush any spurious received characters
   AntSerial.flush();
   
   // Send reset to ANT Network
-  //ant.send(rs);
-  //AP2waitCts();
   ANTreset();
   // Delay after resetting the radio (spec: 500 msec)
-  delay(750);
-  Serial.println("ANT Reset");
-  //parseAntMessage();
-  ANTreceive(100);
+  delay (750);
+  ANTreceive(ANT_RX_MAX_TIMEOUT);
   
   // Set the Network Key (currently to DEVELOPER)
-#if 0
-  snk = SetNetworkKey();
-  snk.setNetwork(0);
-  snk.setKey((uint8_t*)NETWORK_KEY);
-  ant.send(snk);
-#endif
   ANTsetNetwork ();
-  AP2waitCts();
-  ANTreceive (100);
-  //parseAntMessage();
+  ANTreceive (ANT_RX_MAX_TIMEOUT);
 
   // Assign ANT Channel as bi-directional transmitter (Master)
-#if 0
-  ac = AssignChannel();
-  ac.setChannel(0);
-  ac.setChannelType(CHANNEL_TYPE_BIDIRECTIONAL_TRANSMIT); //can't wildcard this
-  ac.setChannelNetwork(0);
-  ant.send(ac);
-#endif
   ANTassignChannel (0, CHANNEL_TYPE_BIDIRECTIONAL_TRANSMIT);
-  AP2waitCts();
-  ANTreceive (100);
-  //parseAntMessage();
+  ANTreceive (ANT_RX_MAX_TIMEOUT);
 
-#if 0
-  ci = ChannelId();
-  ci.setChannel(0);
-  ci.setDeviceNumber(0x0001);
-  ci.setDeviceType(BIKE_POWER_PROFILE);
-  ci.setTransmissionType(BIKE_POWER_TT);
-  ant.send(ci);
-#endif
+  // Set Channel ID to Bike Power profile
   ANTsetChannelId (ANT_CHANNEL_NR, (word) ANT_DEVICE_NR, BIKE_POWER_PROFILE, BIKE_POWER_TT);
-  AP2waitCts();
-  ANTreceive (100);
-  //parseAntMessage();
+  ANTreceive (ANT_RX_MAX_TIMEOUT);
 
-#if 0
-  crf = ChannelRfFrequency();
-  crf.setChannel(0);
-  crf.setRfFrequency(BIKE_POWER_RF); //can't wildcard this
-  ant.send(crf);
-#endif
+  // Set frequency for profile to 4Hz updates
   ANTsetFrequency (ANT_CHANNEL_NR, BIKE_POWER_RF);
-  AP2waitCts();
-  ANTreceive (100);
-  //parseAntMessage();
+  ANTreceive (ANT_RX_MAX_TIMEOUT);
 
-#if 0
-  cp = ChannelPeriod();
-  cp.setChannel(0);
-  cp.setPeriod(BIKE_POWER_CP); //can't wildcard this
-  ant.send(cp);
-#endif
+  // Set ANT period
   ANTsetPeriod (ANT_CHANNEL_NR, BIKE_POWER_CP);
-  AP2waitCts();
-  ANTreceive (100);
-  //parseAntMessage();
+  ANTreceive (ANT_RX_MAX_TIMEOUT);
 
-#if 0
-  oc = OpenChannel();
-  oc.setChannel(0);
-  ant.send(oc);
-#endif
+  // Open the ANT channel for communication broadcasting
   ANTopenChannel (ANT_CHANNEL_NR);
-  AP2waitCts();
-  ANTreceive (100);
-  //parseAntMessage();
+  ANTreceive (ANT_RX_MAX_TIMEOUT);
 }
 
 // *****************************************************************************
@@ -403,90 +332,7 @@ uint8_t buf[13];
   buf[12] = checkSum(buf, 12);
   ANTsend (buf,13);
 
-  AP2waitCts();
-}
-
-// *****************************************************************************
-// ANT stack message parsing
-// *****************************************************************************
-static void parseAntMessage() 
-{
-byte msgId;
-byte *framePtr;
-
-  //delayMicroseconds(100);
-  
-  ant.readPacket();
-  if(ant.getResponse().isAvailable())
-  {
-    msgId = ant.getResponse().getMsgId();
-    switch (msgId) {
-      case CHANNEL_EVENT:
-      {
-        ChannelEventResponse cer = ChannelEventResponse();
-        ant.getResponse().getChannelEventResponseMsg(cer);
-        Serial.println("Received Msg: ChannelEventResponse");
-        Serial.print("Channel: ");
-        Serial.println(cer.getChannelNumber());
-        parseEventMessage(cer.getCode());
-        break;
-      }
-
-      case START_UP_MESSAGE:
-      {
-        StartUpMessage sum = StartUpMessage();
-        ant.getResponse().getStartUpMsg(sum);
-        Serial.println("Received Msg: StartupMessage");
-        Serial.print("Message: ");
-        Serial.println(sum.getMessage());
-        break;
-      }
-
-      default:
-        Serial.print("Ant Message: ");
-        Serial.println(msgId, HEX);
-        framePtr = ant.getResponse().getFrameData();
-        sprintf (TmpBuf, "Ant msg: %02X %02X %02X %02X", framePtr[0], framePtr[1], framePtr[2], framePtr[3]);
-        Serial.println (TmpBuf);
-        
-        break;
-    }
-  }
-  else if (ant.getResponse().isError())
-  {
-    Serial.print("ANT MSG ERROR: ");
-    Serial.println(ant.getResponse().getErrorCode());
-  }
-
-  //delay (10);
-}
-
-// *****************************************************************************
-// ANT stack event message parsing
-// *****************************************************************************
-static void parseEventMessage(uint8_t code)
-{
-BroadcastMsg bm;
-
-  Serial.print("Code: ");
-  switch (code)
-  {
-    case RESPONSE_NO_ERROR:
-      Serial.println("RESPONSE_NO_ERROR");
-      break;
-
-    case EVENT_CHANNEL_CLOSED:
-      Serial.println("EVENT_CHANNEL_CLOSED");
-      break;
-
-    case EVENT_TX:
-      Serial.println("EVENT_TX");
-      break;
-
-    default:
-      Serial.println(code);
-      break;
-  }
+  ANTreceive (ANT_RX_MAX_TIMEOUT);
 }
 
 // *****************************************************************************
@@ -567,22 +413,18 @@ uint32_t retry = 0;
       MsgBuf[MsgIndex] = AntSerial.read();
       if (MsgIndex == MsgLength - 1)
       {
+        for (int i = 0 ; i < MsgLength ; i++)
+        {
+          Serial.print(MsgBuf[i], HEX);
+          Serial.print(" ");
+        }
+        Serial.println(" ");
         // Call the message parser for further action
         ANTrxProcess ();
         MsgSync = false;
       }
     }
- #if 0   
-    else if (MsgSync && MsgIndex == MsgLength)
-    { 
-      MsgIndex++;
-      MsgBuf[MsgIndex] = AntSerial.read();
-      // Call the message parser for further action
-      ANTrxProcess ();
-      MsgSync = false;
-    }
-#endif
-
+ 
     // Wait for next character to parse (can be too fast), max 2 ms (slowest ~4800 baud)
     if (AntSerial.available() == 0)
       delayMicroseconds (2000);
@@ -590,12 +432,6 @@ uint32_t retry = 0;
     sbuflength = AntSerial.available();
   }
  
-  for(int i = 0 ; i < MsgLength ; i++)
-   {
-     Serial.print(MsgBuf[i], HEX);
-     Serial.print(" ");
-   }
-   Serial.println(" ");
 
   // Reset other used variables for next message
   MsgSync = false;
@@ -610,16 +446,17 @@ uint32_t retry = 0;
 /// *****************************************************************************
 static void ANTrxProcess (void)
 {
-  Serial.println ("ANTrxProcess");
   // Parse the Channel response messages
   if (MsgBuf[2] == MSG_CHANNEL_RESPONSE)
   {
-    // RF Event is code 1
+    // An RF Event is code 1
     if (MsgBuf[4] == 0x01)
     {
       if (MsgBuf[5] == MSG_TX_EVENT)
       {
+        digitalWrite(LED_BLUE, !digitalRead (LED_BLUE));
         Serial.println("TX success");
+#if 0
         if (SendCount < 4)
         {
           //broadcastBikePower();
@@ -633,70 +470,60 @@ static void ANTrxProcess (void)
           SendCount = 0;
           //Serial.println("Basic Power");
         }
-        
+#endif        
       }
     }
-    else if (MsgBuf[4] == MSG_NETWORK_KEY_ID)
+    else
     {
-      Serial.print ("Set Network Key: ");
+      switch (MsgBuf[4])
+      {
+        case MSG_NETWORK_KEY_ID:
+          Serial.print ("Set Network Key: ");
+        break;
+  
+        case MSG_ASSIGN_CHANNEL_ID:
+          Serial.print ("Assign Channel: ");
+        break;
+        
+        case MSG_CHANNEL_ID_ID:
+          Serial.print ("Set Channel ID: ");
+        break;
+        
+        case MSG_CHANNEL_RADIO_FREQ_ID:
+          Serial.print ("Set Frequency: ");
+        break;
+       
+        case MSG_CHANNEL_MESG_PERIOD_ID:
+          Serial.print ("Set Period: ");
+        break;
+       
+        case MSG_RADIO_TX_POWER_ID:
+          Serial.print ("Set Tx Power: ");
+        break;
+       
+        case MSG_CHANNEL_SEARCH_TIMEOUT_ID:
+          Serial.print ("Set Timout: ");
+        break;
+      
+        case MSG_OPEN_CHANNEL_ID:
+          Serial.print ("Open Channel: ");
+        break;
+        
+        default:
+          Serial.print ("Unknown Msg: ");
+        break;
+      }
+      
       if (MsgBuf[5] == 0)
         Serial.println("No Error");
       else
-        Serial.println(MsgBuf[4], HEX);
-    }
-    else if (MsgBuf[4] == 0x42) // Assign Channel Assigned
-    {
-      if (MsgBuf[5] == 0)
       {
-        Serial.println("Assign Channel: No Error");
-      }
-    }
-    else if (MsgBuf[4] == 0x51) // Set Channel ID
-    {
-      if (MsgBuf[5] == 0)
-      {
-        Serial.println("Set Channel ID: No Error");
-      }      
-    }
-    else if (MsgBuf[4] == 0x45) // Set Frequency
-    {
-      if (MsgBuf[5] == 0)
-      {
-        Serial.println("Set Frequency: No Error");
-      }      
-    }
-    else if (MsgBuf[4] == 0x43 ) // Set Period
-    {
-      if (MsgBuf[5] == 0)
-      {
-        Serial.println("Set Period: No Error");
-      }      
-    }
-    else if (MsgBuf[4] == 0x47 ) //Transmit Power
-    {
-      if (MsgBuf[5] == 0)
-      {
-        Serial.println("Transmit Power: No Error");
-      }      
-    }
-    else if (MsgBuf[4] == 0x44 ) //Set Timeout
-    {
-      if (MsgBuf[5] == 0)
-      {
-        Serial.println("Set Timeout: No Error");
-      }      
-    }
-    else if (MsgBuf[4] == 0x4B ) //Open Channel
-    {
-      if (MsgBuf[5] == 0)
-      {
-        Serial.println("Open Channel: No Error");
-      }      
+        Serial.print ("ERR: ");
+        Serial.println(MsgBuf[5], HEX);
+      }  
     }
   }
- 
- }
-
+}
 
 // *****************************************************************************
 // ANT AP2 send routine(s)
@@ -833,7 +660,6 @@ uint8_t buf[7];
     buf[6] = checkSum(buf, 6);
     ANTsend (buf,7);
 }
-
 
 // *****************************************************************************
 // ANT setup routine(s); ANTopenChannel
